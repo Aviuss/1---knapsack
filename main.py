73e60030d89a223f3340c4889de7d88b80babb6d
Aviuss <konradbb09@gmail.com>
2026-03-27 14:11:36 +0100

    read file data

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import argparse
 
+def read_data_from_file(filepath):
+    file = open(filepath, 'r')
+    data = file.readlines()
+    file.close()
+    data = list(map(lambda x:x.split(' '), data))
+    data = list(map(lambda x:[int(x[0]), int(x[1])], data))
+    return {
+        "n": data[0][0],
+        "W": data[0][1],
+        "list_price_weight": data[1:]
+    }
+
 def main(args):
-    print(args)
-    print(args.file)
+    knapsack_data = read_data_from_file(args.file)
+    print(knapsack_data)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
